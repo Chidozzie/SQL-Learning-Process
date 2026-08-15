c5439b316ec29ce04e7e78319b37d35f947e4bba
Chidozzie <150841313+Chidozzie@users.noreply.github.com>
2026-08-15 21:05:03 +0100

    week1-avg-loan-amount-by-status.sql

--- week1-avg-loan-amount-by-status.sql
+++ week1-avg-loan-amount-by-status.sql
@@ -1,5 +1,5 @@
 SELECT "Loan Status", ROUND(AVG("Current Loan Amount"),2) AS Average_Loan_Amount
 FROM loans
-WHERE "Loan Status" IS NOT NULL
+WHERE "Loan Status" IS NOT NULL AND "Current Loan Amount" < 99999999
 GROUP BY "Loan Status"
 ORDER BY Average_Loan_Amount DESC;
